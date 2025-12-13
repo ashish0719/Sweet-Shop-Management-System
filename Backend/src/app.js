5b8b7a227e257ddcba3f0cd33d69b5bceb2933c3
ashish0719 <sauravashish.0000@gmail.com>
2025-12-13 23:10:29 +0530

    feat(auth): implement user login endpoint (TDD green)

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const connectDB = require("./config/db");
 
 const app = express();
-
-connectDB();
 
 app.use(express.json());
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Sweet Shop Backend!");
+});
 
 module.exports = app;
